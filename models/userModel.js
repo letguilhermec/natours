ce5336fa9f515fcca048795637967a27a46ee880
Guilherme Correa <correacgui@gmail.com>
2022-08-29 17:40:48 -0300

    Added User Schema

--- models/userModel.js
+++ models/userModel.js
@@ -5,31 +5,38 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Please tell us your name"],
+      required: [true, 'Please tell us your name.'],
       /*minlength: [5, "User name must have at least 5 characters"],
       maxlength: [50, "User name must have at most 50 characters"]*/
     },
     email: {
       type: String,
-      required: [true, "Please tell us your email"],
+      required: [true, 'Please tell us your email.'],
       unique: true,
       lowercase: true,
-      validate: {
-        validator: 
-      },
+      validate: [validator.isEmail, 'Please provide a valid email.'],
       /*minlength: [],
       maxlength: [],*/
     },
-    photo: {
-      type: '??',
-    },
+    photo: String,
     password: {
       type: String,
-      required: true,
+      required: [true, 'Please provide a password.'],
+      minlength: [8, 'Password must have at least 8 characters.'],
     },
     passwordConfirm: {
       type: String,
-      required: true,
-    }
+      required: [true, 'Please confirm the password.'],
+      validator: {
+        validate: function(val) {
+          return val = this.password
+        },
+        message: 'Passwords must match.',
+      },
+    },
   }
 )
+
+const User = mongoose.model(userSchema)
+
+module.exports = User
