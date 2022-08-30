3c138d7d7880e9bfc27818e9e2c7c6aa449d82b7
Guilherme Correa <correacgui@gmail.com>
2022-08-30 19:07:53 -0300

    Added data sanitization -> XSS and query injection

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 const morgan = require('morgan')
 const AppError = require('./utils/appError')
 const errorHandler = require('./controllers/errorController')
@@ -29,6 +31,11 @@ app.use('/api', limiter)
 
 //  Body parser -> reads data from req.body
 app.use(express.json({ limit: '10kb' }))
+
+//  Data sanitization against noSQL query injection
+app.use(mongoSanitize())
+//  Data sanitization against XSS injection
+app.use(xss())
 
 //  Serve static files
 app.use(express.static(`${__dirname}/public`))
