9cf56e7be885f44f587a9b0f5a5cc9ea81eadf43
Guilherme Correa <correacgui@gmail.com>
2022-08-31 13:13:03 -0300

    Populate tours w/ reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,10 +34,10 @@ const reviewSchema = new mongoose.Schema(
 )
 
 reviewSchema.pre(/^find/, function(next) {
-  this.populate({
+  this/*.populate({
     path: 'tour',
     select: 'name'
-  })
+  })*/
     .populate({
       path: 'user',
       select: 'name photo'
