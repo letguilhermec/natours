e5fdc1256fe71eb24918a46584ce5a2435bc4d80
Guilherme Correa <correacgui@gmail.com>
2022-09-01 22:06:09 -0300

    Final draft of tour view

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,19 +2,27 @@ const Tour = require('../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
 
 exports.getOverview = catchAsync(async (req, res) => {
-  //  Get All Tours from API
+  //  Get All Tours from collection
   const tours = await Tour.find()
 
-  //  Render template using data from API
+  //  Render template using data
   res.status(200).render('overview', {
     title: 'All Tours',
     tours
   })
 })
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  //  Get the data for requested tour
+  const tour = await Tour
+    .findOne({ slug: req.params.tourSlug })
+    .populate({
+      path: 'reviews',
+      fields: 'review rating user'
+    })
+  // Render the template using data
   res.status(200).render('tour', {
     tour,
     title: `${tour.name} Tour`
   })
-}
+})
