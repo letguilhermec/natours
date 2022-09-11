8ad75154770be84ded0a374a01b4812cdfeac649
Guilherme Correa <correacgui@gmail.com>
2022-09-11 20:13:44 -0300

    Added compression module to compress text responses

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
 const hpp = require('hpp')
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+const compression = require('compression')
 
 const cors = require('cors')
 
@@ -72,6 +72,8 @@ app.use(
     ]
   })
 )
+
+app.use(compression())
 
 /*app.use((req, res, next) => {
   console.log(req.cookies)
