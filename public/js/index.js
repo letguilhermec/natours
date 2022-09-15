13e46efd25a50bfb46a41cdaa33db02d99a05222
Guilherme Correa <correacgui@gmail.com>
2022-09-15 13:39:13 -0300

    Trying to fix Stripe

--- public/js/index.js
+++ public/js/index.js
@@ -84,5 +84,5 @@ if (bookBtn) {
   })
 }
 
-const alertMessage = document.querySelector('body').dataset.alerts
+const alertMessage = document.querySelector('body').dataset.alert
 if (alertMessage) showAlert('success', alertMessage, 20)
