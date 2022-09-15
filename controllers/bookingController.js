3067ae0176c82fcbf519fb2632c4bfee61cb2c01
Guilherme Correa <correacgui@gmail.com>
2022-09-15 13:59:37 -0300

    Trying to fix Stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -63,6 +63,7 @@ const createBookingCheckout = catchAsync(async session => {
   const tour = session.client_reference_id
   const user = await User.findOne({ email: session.customer_email }).id
   const price = session.amount_total / 100
+  console.log(session)
 
   await Booking.create({ tour, user, price })
 })
